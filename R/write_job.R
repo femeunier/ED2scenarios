c3f254c54b71a03c867558349321c445960a3ed1
FULLNAME <you@yourdomain.example.com>
2020-07-09 08:42:52 -0400

    Change R function

--- R/write_job.R
+++ R/write_job.R
@@ -4,13 +4,14 @@ write_job <- function(file = file.path(getwd(),"job.sh"),
                       CD = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/run",
                       ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/run/ed_2.1-opt",
                       ED2IN = "ED2IN",
-                      Rplot_function = '/data/gent/vo/000/gvo00074/felicien/R/read_and_plot_ED2_Q2R.r'){
+                      Rplot_function = '/data/gent/vo/000/gvo00074/felicien/R/read_and_plot_ED2_Q2R_tspft.r'){
 
   ed2in <- read_ed2in(file.path(CD,ED2IN))
   DN <- dirname(ed2in$FFILOUT)
   analy <- basename(ed2in$FFILOUT)
   init <- paste(ed2in$IYEARA,sprintf('%02d',ed2in$IMONTHA),sprintf('%02d',ed2in$IDATEA),sep='/')
   end <- paste(ed2in$IYEARZ,sprintf('%02d',ed2in$IMONTHZ),sprintf('%02d',ed2in$IDATEZ),sep='/')
+  Rfunction <- tools::file_path_sans_ext(basename(Rplot_function))
 
   writeLines("#!/bin/bash -l",con = file)
   write(paste0("#PBS -l nodes=",nodes,":ppn=",ppn),file=file,append=TRUE)
@@ -24,7 +25,7 @@ write_job <- function(file = file.path(getwd(),"job.sh"),
   write(paste(ed_exec,"-f",ED2IN),file=file,append=TRUE)
   write("",file=file,append=TRUE)
   write(paste0("echo \"source(\'",Rplot_function,"\')"),file=file,append=TRUE)
-  write(paste0("read_and_plot_ED2_Q2R(\'",DN,"\',\'",analy,"\',\'",init,"\',\'",end,"\')"),file=file,append=TRUE)
+  write(paste0(Rfunction,"(\'",DN,"\',\'",analy,"\',\'",init,"\',\'",end,"\')"),file=file,append=TRUE)
   write("\" | R --vanilla",file=file,append=TRUE)
 }
 
