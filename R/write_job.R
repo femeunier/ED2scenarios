d8b10ca03c977d516428711e6ec806f5e9de8176
femeunier <felicien.meunier@gmail.com>
2021-12-16 10:05:25 +0100

    Add clean option

--- R/write_job.R
+++ R/write_job.R
@@ -4,7 +4,8 @@ write_job <- function(file = file.path(getwd(),"job.sh"),
                       CD = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/run",
                       ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/run/ed_2.1-opt",
                       ED2IN = "ED2IN",
-                      Rplot_function = '/data/gent/vo/000/gvo00074/felicien/R/read_and_plot_ED2_Q2R_tspft.r'){
+                      Rplot_function = '/data/gent/vo/000/gvo00074/felicien/R/read_and_plot_ED2_Q2R_tspft.r',
+                      clean = FALSE){
 
   ed2in <- read_ed2in(file.path(CD,ED2IN))
   DN <- dirname(ed2in$FFILOUT)
@@ -23,6 +24,15 @@ write_job <- function(file = file.path(getwd(),"job.sh"),
   write(paste("cd",CD),file=file,append=TRUE)
   write("",file=file,append=TRUE)
   write(paste(ed_exec,"-f",ED2IN),file=file,append=TRUE)
+  write("",file=file,append=TRUE)
+
+  if (clean){
+    ed2in <- read_ed2in(file.path(CD,ED2IN))
+    OPfiles <- ed2in$FFILOUT
+    CMD <- paste0("rm $(find ",dirname(OPfiles)," -name '*' ! -name '",paste0(basename(OPfiles),"-Q*-","01","-*"),"')")
+    write(CMD,file=file,append=TRUE)
+  }
+
   write("",file=file,append=TRUE)
   write(paste0("echo \"source(\'",Rplot_function,"\')"),file=file,append=TRUE)
   write(paste0(Rfunction,"(\'",DN,"\',\'",analy,"\',\'",init,"\',\'",end,"\')"),file=file,append=TRUE)
