f49061e2150fea0cadc379d83c88ae7daf655dc8
femeunier <felicien.meunier@gmail.com>
2022-03-22 16:57:15 +0100

    Handle multiple job name

--- R/write_bash_submission.R
+++ R/write_bash_submission.R
@@ -4,9 +4,11 @@ write_bash_submission <- function(file = file.path(getwd(),"all_jobs.sh"),
 
   writeLines("#!/bin/bash -l",con = file)
 
+  if (length(job_name) == 1) job_name <- rep(job_name,length(list_files))
+
   tryCatch(map(1:length(list_files),function(i){
     write(paste("cd",list_files[[i]]),file=file,append=TRUE)
-    write(paste("qsub",job_name),file=file,append=TRUE)
+    write(paste("qsub",job_name[i]),file=file,append=TRUE)
   }),error = function(e) e)
 
   return(TRUE)
